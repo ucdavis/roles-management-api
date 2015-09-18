3a0a0606d2a2e198bec12a3b4d1abd0816c3e3f6
Christopher Thielen <cmthielen@ucdavis.edu>
2015-09-18 13:11:10 -0700

    Added person.role_assignments and person.group_memberships

--- lib/roles-management-api/person.rb
+++ lib/roles-management-api/person.rb
@@ -1,12 +1,14 @@
 module RolesManagementAPI
   class Person
-    attr_accessor :id, :loginid, :name
+    attr_accessor :id, :loginid, :name, :role_assignments, :group_memberships
 
     # Creates a new Person object from a JSON object
     def initialize(json)
       @id = json[:id]
       @loginid = json[:loginid]
       @name = json[:name]
+      @role_assignments = json[:role_assignments]
+      @group_memberships = json[:group_memberships]
     end
 
     def as_json
